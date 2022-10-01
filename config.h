a65323a454669aacceb4dc89d1ce2e354809350c
Matvey <mathway.home@gmail.com>
2022-10-01 22:51:46 +0300

    updated something

--- config.h
+++ config.h
@@ -112,8 +112,8 @@ static const char *scrn_full_clipcmd[] = {"screenshot", "-c", "-f", NULL}; /* Se
 static const char *scrn_part_clipcmd[] = {"screenshot", "-c", "-p", NULL}; /* Self-written script */
 
 /* Miscellaneous commands */
-static const char *toggle_lft_scrn_cmd[] = {"screenctl", "--command", "toggle", "--output", "DP2", NULL}; /* Self-written script */
-static const char *toggle_rt_scrn_cmd[] = {"screenctl", "--command", "toggle", "--output", "eDP1", NULL}; /* Self-written script */
+static const char *toggle_lft_scrn_cmd[] = {"screenctl", "--command", "toggle", "--output", "DP-2", NULL}; /* Self-written script */
+static const char *toggle_rt_scrn_cmd[] = {"screenctl", "--command", "toggle", "--output", "e-DP1", NULL}; /* Self-written script */
 
 
 #include "shift-tools.c"
